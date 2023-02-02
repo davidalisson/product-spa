ab0404a13bcd7bb54fc4ae5b1d6536976a69871d
David Alisson G. Lopes <davidglopes00@gmail.com>
2023-02-02 11:01:11 -0300

    Product service documentation

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../classes/product';
-import { EMPTY, empty, Observable, of } from 'rxjs';
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { EMPTY_OBSERVER } from 'rxjs/internal/Subscriber';
+import { EMPTY, Observable } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -12,69 +10,105 @@ import { MessageService } from './message.service';
 export class ProductService {
   baseUrl = 'http://localhost:3000/produtos';
 
-  constructor(private http: HttpClient, private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
+  /**
+   * Essa função cria um novo produto na base, verificando antes da criação se o código é único ou não
+   * @param p objeto de produto
+   * @returns retorna sucesso caso o produto seja inserido, ou retorna vazio caso o código já exista na base
+   */
   async create(p: Product): Promise<Observable<Product>> {
     let codeExists = false;
-    const p1 = await new Promise((resolve, reject)=>{
+    const p1 = await new Promise((resolve, reject) => {
       this.getByCodigo(p.codigo).subscribe({
-        next: (res: Product[])=>{
-          if(res.length>0){
+        next: (res: Product[]) => {
+          if (res.length > 0) {
             codeExists = true;
             resolve(codeExists);
           } else {
             codeExists = false;
             resolve(codeExists);
           }
-        }
+        },
       });
-    }).then(val=>{});
-    if(!codeExists){
+    }).then((val) => {});
+    if (!codeExists) {
       return this.http.post<Product>(this.baseUrl, p);
-    }
-    else{
-      this.messageService.showWarning('Código de produto já existente.', 'Atenção')
+    } else {
+      this.messageService.showWarning(
+        'Código de produto já existente.',
+        'Atenção'
+      );
       return EMPTY;
     }
-
   }
-
+  /**
+   * Essa função get retorna todos os produtos
+   * @returns retorna todos os produtos
+   */
   getAll(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
   }
 
+  /**
+   * Essa função get retorna um produtos pelo código
+   * @param codigo código do produto
+   * @returns retorna o produto pelo código
+   */
   getByCodigo(codigo: string): Observable<Product[]> {
     let url = `${this.baseUrl}?codigo=${codigo}`;
     return this.http.get<Product[]>(url);
   }
 
-  getByCodigoAndCategoria(codigo: string, categoria: string): Observable<Product[]> {
+  /**
+   * Essa função get retorna produtos de acordo com os parâmetros passados
+   * @param codigo código do produto
+   * @param categoria categoria do produto
+   * @returns retorna uma lista de produtos de acordo com o código e a categoria informada
+   */
+  getByCodigoAndCategoria(
+    codigo: string,
+    categoria: string
+  ): Observable<Product[]> {
     let url;
-    if(codigo && categoria && !categoria.includes('TODOS')){
+    if (codigo && categoria && !categoria.includes('TODOS')) {
       url = `${this.baseUrl}?codigo=${codigo}&categoria=${categoria}`;
-    } else if(!codigo && categoria && !categoria.includes('TODOS')){
+    } else if (!codigo && categoria && !categoria.includes('TODOS')) {
       url = `${this.baseUrl}?categoria=${categoria}`;
-    } else if(!codigo && categoria && categoria.includes('TODOS')){
+    } else if (!codigo && categoria && categoria.includes('TODOS')) {
       url = `${this.baseUrl}`;
-    }else{
+    } else {
       url = `${this.baseUrl}?codigo=${codigo}`;
     }
     return this.http.get<Product[]>(url);
   }
 
+  /**
+   * Essa função get retorna um produtos pelo id
+   * @param id id do produto
+   * @returns retorna o produto pelo id
+   */
   getById(id: number): Observable<Product[]> {
     let url = `${this.baseUrl}?id=${id}`;
     return this.http.get<Product[]>(url);
   }
 
+  /**
+   * Essa função atualiza o produto na base, verificando antes da atualização se o código é único ou não
+   * @param produto objeto de produto
+   * @returns retorna sucesso caso o produto seja inserido, ou retorna vazio caso o código já exista na base
+   */
   async update(produto: Product): Promise<Observable<Product>> {
     let url = `${this.baseUrl}/${produto.id}`;
     let codeExists = false;
-    const p1 = await new Promise((resolve, reject)=>{
+    const p1 = await new Promise((resolve, reject) => {
       this.getByCodigo(produto.codigo).subscribe({
-        next: (res: Product[])=>{
-          if(res.length>0){
-            if(res[0].id === produto.id){
+        next: (res: Product[]) => {
+          if (res.length > 0) {
+            if (res[0].id === produto.id) {
               codeExists = false;
               resolve(codeExists);
             } else {
@@ -85,18 +119,25 @@ export class ProductService {
             codeExists = false;
             resolve(codeExists);
           }
-        }
+        },
       });
-    }).then(val=>{});
-    if(!codeExists){
+    }).then((val) => {});
+    if (!codeExists) {
       return this.http.put<Product>(url, produto);
-    }
-    else{
-      this.messageService.showWarning('Código de produto já existente.', 'Atenção')
+    } else {
+      this.messageService.showWarning(
+        'Código de produto já existente.',
+        'Atenção'
+      );
       return EMPTY;
     }
   }
 
+  /**
+   * Essa função deleta o produto na base
+   * @param id id do produto
+   * @returns retorna sucesso caso o produto seja deletado
+   */
   delete(id: number): Observable<Product> {
     let url = `${this.baseUrl}/${id}`;
     return this.http.delete<Product>(url);
